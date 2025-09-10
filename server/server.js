7f1e2693641913805f689a5170b324c078e541dd
SaiVardhan2007 <polampallisaivardhan2007@gmail.com>
2025-09-11 01:33:56 +0530

    3rd commit

--- server/server.js
+++ server/server.js
@@ -9,11 +9,9 @@ const app = express();
 
 await connectDB()
 
-// Middlewares
 app.use(cors())
 app.use(express.json())
 
-// Routes
 app.get('/', (req, res)=> res.send("API is Working"))
 app.use('/api/admin', adminRouter)
 app.use('/api/blog', blogRouter)
